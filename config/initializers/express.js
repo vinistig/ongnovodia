b9aa5d55349fcd3d55893568f9a5af50f84871b2
Vinicius Stigliani <vinistig@br.ibm.com>
2017-01-22 23:55:06 -0200

    fix required fields and cors

--- config/initializers/express.js
+++ config/initializers/express.js
@@ -23,6 +23,19 @@ function bootExpress(config) {
 	app.use('/', routes);
 
 	app.listen(config.port, () => { console.log('Express is up.'); });
+	
+	app.all('/*', function(req, res, next) {
+	   //Set up CORS headers
+	   res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+	   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	   // Set custom headers for CORS
+	   res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-type,Accept,X-Access-Token,X-Key');
+	   if (req.method == 'OPTIONS') {
+	       res.status(200).end();
+	   } else {
+	       next();
+	  }
+  });
 }
 
 module.exports = bootExpress;
